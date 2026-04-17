aa44e3381be9eaff00cd664d55ad69e9e57aab3c
sTrenk0 <Artem3930@proton.me>
2026-04-17 21:05:47 +0200

    add AuthModule with register, login, JWT

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Module({
+  imports: [
+	UsersModule,
+	JwtModule.registerAsync({
+  	imports: [ConfigModule],
+  	inject: [ConfigService],
+  	useFactory: (config: ConfigService) => ({
+    	secret: config.get<string>('JWT_SECRET'),
+    	signOptions: {
+      	expiresIn: config.get<string>('JWT_EXPIRES_IN', '1h') as `${number}${'s' | 'm' | 'h' | 'd'}`,
+    	},
+  	}),
+	}),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [JwtModule],
+})
+export class AuthModule {}
